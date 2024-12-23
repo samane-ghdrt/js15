72aa9371082fff67e1e13678fd08b0f810a4e5e5
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-22 23:08:03 -0900

    add

--- exercise4/colors.js
+++ exercise4/colors.js
@@ -15,4 +15,57 @@ rgba(223, 4, 70, 1)
 rgba(127, 31, 85, 1) 
 rgba(93, 18, 199, 1)
 rgba(93, 18, 199, 1)
+
 */
+const r1El =document.querySelector('r1');
+const r2El =document.querySelector('r2');
+const r3El =document.querySelector('r3');
+const r4El =document.querySelector('r4');
+const r5El =document.querySelector('r5');
+const cirEl =document.querySelectorAll('.circle');
+cirEl.remove.addEventListener('click',(element)=>{
+    if(element.classList.contains('c1')){
+        r1El.style.backgroundColor=rgb(162, 237, 217,1);
+        r1E2.style.backgroundColor=rgb(20, 172, 132,1);
+        r1E3.style.backgroundColor=rgb(103, 158, 143,1);
+        r1E4.style.backgroundColor=rgb(31, 214, 165,1);
+        r1E5.style.backgroundColor=rgb(11, 86, 66,1);
+       } else if(element.classList.contains('c2')){
+           r1El.style.backgroundColor=rgb(243, 229, 180,1);
+           r1E2.style.backgroundColor=rgb(236, 215, 140,1);
+           r1E3.style.backgroundColor=rgb(241, 212, 107,1);
+           r1E4.style.backgroundColor=rgb(156, 129, 33,1);
+           r1E5.style.backgroundColor=rgb(119, 101, 37,1);
+          } else if(element.classList.contains('c3')){
+            r1El.style.backgroundColor=rgb(242, 206, 173,1);
+            r1E2.style.backgroundColor=rgb(220, 178, 138,1);
+            r1E3.style.backgroundColor=rgb(247, 173, 103,1);
+            r1E4.style.backgroundColor=rgb(230, 158, 90,1);
+            r1E5.style.backgroundColor=rgb(207, 124, 47,1);
+           } else if(element.classList.contains('c4')){
+            r1El.style.backgroundColor=rgb(238, 169, 169,1);
+            r1E2.style.backgroundColor=rgb(226, 126, 126,1);
+            r1E3.style.backgroundColor=rgb(235, 92, 92,1);
+            r1E4.style.backgroundColor=rgb(194, 70, 70,1);
+            r1E5.style.backgroundColor=rgb(163, 49, 49,1);
+           } else if(element.classList.contains('c5')){
+            r1El.style.backgroundColor=rgb(199, 134, 154,1);
+            r1E2.style.backgroundColor=rgb(222, 109, 143,1);
+            r1E3.style.backgroundColor=rgb(197, 86, 119,1);
+            r1E4.style.backgroundColor=rgb(99, 21, 44,1);
+            r1E5.style.backgroundColor=rgb(223, 4, 70,1);
+           } else if(element.classList.contains('c6')){
+            r1El.style.backgroundColor=rgb(243, 229, 180,1);
+            r1E2.style.backgroundColor=rgb(236, 215, 140,1);
+            r1E3.style.backgroundColor=rgb(241, 212, 107,1);
+            r1E4.style.backgroundColor=rgb(156, 129, 33,1);
+            r1E5.style.backgroundColor=rgb(119, 101, 37,1);
+           } else if(element.classList.contains('c7')){
+            r1El.style.backgroundColor=rgb(243, 229, 180,1);
+            r1E2.style.backgroundColor=rgb(236, 215, 140,1);
+            r1E3.style.backgroundColor=rgb(241, 212, 107,1);
+            r1E4.style.backgroundColor=rgb(156, 129, 33,1);
+            r1E5.style.backgroundColor=rgb(119, 101, 37,1);
+           }
+       
+    })
